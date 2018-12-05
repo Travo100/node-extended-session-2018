1bfe0f8c51c7e62fd3b0fcef7f8fca7894e6ef21
Travo100 <travo100@gmail.com>
2018-12-04 17:31:46 -0800

    Added better user input validation and can search any movie on omdb api

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 // including the fs or filesystem package
 var fs = require("fs");
 
+var axios = require("axios");
 // take in a user input
 var userInput = process.argv[2];
-var userText = process.argv[3];
+var userText = "";
+
+for(var i = 3; i < process.argv.length; i++) {
+    userText += process.argv[i] + " ";
+}
+
+var queryUrl = `http://www.omdbapi.com/?t=${userText}&y=&plot=short&apikey=trilogy`;
+console.log(queryUrl);
 
 // let's the user decide if they would like to read a file "read"
 if(userInput === "read") {
@@ -26,12 +34,26 @@ if(userInput === "read") {
 } else if (userInput === "append") {
     // or append to a file "append"
     
-    fs.appendFile("myFile.txt", userText, function(err){
+    appendTextToFile(userText);
+
+} else if (userInput === "movie-this") {
+    // hit the url with axios 
+    axios.get(queryUrl).then(function(response){
+        // and display data back from a movie
+        console.log(response.data);
+        var movieObj = response.data;
+        appendTextToFile(movieObj.Plot);
+    });
+    
+} else {
+    console.log("No command found!");
+}
+
+function appendTextToFile(text) {
+    fs.appendFile("myFile.txt", text, function(err){
         if(err) {
             return console.log(err);
         }
         console.log("appending to a file"); 
     });
-} else {
-    console.log("No command found!");
 }
